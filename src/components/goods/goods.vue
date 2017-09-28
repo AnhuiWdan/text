e85f4cace808c93d1a5745b39d6aeb5f68594231
王丹 <wangdan@wangdandeiMac.local>
2017-09-28 21:58:49 +0800

    mac

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="goods">
+    I am goods
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="header">
+    我是header
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .header
+    font-size: 12px
+</style>
