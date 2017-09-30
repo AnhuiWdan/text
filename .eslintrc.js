c7639cd23697080d439cbaba558679e3284e50df
wangdan <183048460@qq.com>
2017-09-30 17:07:37 +0800

    修改配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 函数定义时括号前面要不要有空格
     'space-before-function-paren': 0
   }
 }
